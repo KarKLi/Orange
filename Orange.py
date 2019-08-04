498fcc0870a29fcb83f87616f8ab1490a835bb49
KarK Li <34907015+KarKLi@users.noreply.github.com>
2019-08-04 21:01:52 +0800

    Add files via upload

--- Orange.py
+++ Orange.py
@@ -16,15 +16,17 @@ except ImportError:
     raise ImportError('Please install TensorFlow first.')
 
 class VersionError(BaseException):
-    """
-    Just an error, never mind.
+    """Just an error, never mind.
+"""
+    pass
+class ExecutionError(BaseException):
+    """Just an error, never mind.
 """
     pass
 
 class __Orange(object):
     def __init__(self,model_name,input_shape):
-        """
-    __init__ accept two parameters, model_name and input_shape,
+        """__init__ accept two parameters, model_name and input_shape,
     @ param:
     model_name : Model's name, such as ResNet-18, ResNet-34. This model name is a symbol to recognize the model.
     input_shape : Regulate the input vector's shape.
@@ -116,6 +118,88 @@ class __Orange(object):
     """
         return tf.keras.optimizers.Adam(lr,beta_1,beta_2,epslion,decay.amsgrad)
 
+    def __MSE():
+        """Mean squared error (MSE) is a loss function used in model complication.
+    @ formula:
+    MSE(bar(theta))=E(bar(theta)-theta)^2
+    After equal transformation, the MSE is equal to D(theta)+[E(theta)-theta)]^2
+
+    MSE has two parts, first is the theta's deviation, and the second is the distance between exception value and the truth value.
+    It is L2 loss.
+    If the estimation of theta is unbiased estimate, then the MSE(theta) is equal to its deviation.
+    """
+        return tf.keras.losses.mean_squared_error
+
+    def __MAE():
+        """Mean absolute error (MAE) is a loss function used in model complication.
+    @ formula:
+    for a set of data {x_1,x_2,...,x_n}, the MAE is defined as:
+    MAE=1/n*(\sigma_{i=1}^n{x_i-m(x)}) which the m(x) is the truth value.
+
+    MAE is L1 loss, it treat all deviation as equal and doesn't take harsh punishment to the data which has large deviation.
+    """
+        return tf.keras.losses.mean_absolute_error
+    
+    def __MAPE():
+        """Mean absolute percentage error (MAPE) is a loss function used in model complication.
+    @ formula:
+    MAPE=100%/n*(\sum_{i=1}^n{(bar(y_i)-y_i)/y_i}
+
+    MAPE is a loss ratio of all data. If MAPE=0%, the model is a perfect model. If MAPE >= 1, the model is an awful model.
+    """
+        return tf.keras.losses.mean_absolute_percentage_error
+    
+    def __hinge(squared=False,categorical=False):
+        """Hinge loss is a loss function used in model complication.
+    @ formula:
+    min_{W,b}\sum_{i=1}^N\sum_{j not equal to y_i}max(0,s_j-s_{y_i}+1)+\lambda \sum_k \sum_n W_{k,n}^2
+
+    Hinge loss always used in SVM task. If you want to use squared hinge loss, set the sqaured parameter as True.
+    If you want to use categorical hinge loss, set the categorical parameter as True.
+    DON'T set the parameters True at the same time!
+    """
+        if squared is False and categorical is False:
+            return tf.keras.losses.hinge
+        elif squared is True and categorical is False:
+            return tf.keras.losses.squared_hinge
+        elif squared is False and categorical is True:
+            return tf.keras.losses.categorical_hinge
+        else:
+            raise ValueError('Not sure what hinge loss you need to use ! (Squared hinge or Categorical hinge?)')
+
+    def __logcosh():
+        """Log-cosh loss function is a loss function used in model complication.
+    @ formula:
+    L(y,y^p)=\sum_{i=1}^n log(cosh(y_i^p-y_i))
+
+    It has this mathematical nature:
+    log(cosh(x)) \t equals to x^2/2         \t when x is small
+                 \t equals to abs(x)-log(2) \t when x is large
+    logcosh is approximately equal to MSE and decay the crazy prediction's effect.
+    """
+        return tf.keras.losses.logcosh
+
+    def __crossentropy(Sigmoid=False,Softmax=False,Sparse=False,y_labels=None):
+        """Cross entropy loss function is a loss function used in model complication.
+    @ param:
+    Sigmoid -- Will use binary_crossentropy if the activate function is Sigmoid
+    Softmax -- Will use categorical_crossentropy if the activate function is Softmax
+    Sparase -- Will use sparse_categorical_crossentropy if the dataset has many zero data.
+
+    You don't need to do preprocess of the label. At categorical_crossentropy and sparse_categorical_crossentropy, the crossentropy function
+    will do the preprocessing. But you need to pass the y_labels to process.
+    """
+        if Sigmoid is True:
+            return tf.keras.losses.binary_crossentropy
+        elif Softmax is True and y_labels is not None:
+            from keras.utils.np_utils import to_categorical
+            y_labels=to_categorical(y_labels,num_classes=None)
+            return tf.keras.losses.categorical_crossentropy
+        else:
+            import numpy as np
+            y_labels=np.expand_dims(y_labels,-1)
+            return tf.keras.losses.sparse_categorical_crossentropy
+
     def GaussianNoise(self,stddev,input_shape=None):
         """Gaussian Noise layer. Add a noise which its distribution follow the Gaussian distribution.
     @ formula:
@@ -148,12 +232,15 @@ class __Orange(object):
             try:
                 self.__model.get_layer(index=0)
             except ValueEroor:
-                raise ValueError('The model doesn\'t have any layer. Set the input_shape if you want to add this layer as the first layer of the network.')
+                if self.__shape is None:
+                    raise ValueError('The model doesn\'t have any layer. Set the input_shape if you want to add this layer as the first layer of the network.')
+                else:
+                    self.__model.add(tf.keras.layers.GaussianDropout(rate,input_shape=self.__shape))
         else:
             self.__model.add(tf.keras.layers.GaussianDropout(rate,input_shape=input_shape))
 
     """
-    Second part of the Orange library : layers in neural network.
+    Second part of the Orange library : common layers in neural network.
     Include fully-connected layer, flatten layer (preprocess layer), batch normalization, pooling and convolution layer.
     At most of fully-connected layer, the activate function is:relu,leaky relu, tanh, softmax and sigmoid.
     The Orange only provide convolution operation which the kernel is two-dimensional.
@@ -201,8 +288,7 @@ class __Orange(object):
         self.__model.add(tf.keras.layers.Dense(neural_numbers,activation=tf.nn.leaky_relu))
 
     def tanh(self,neural_numbers):
-        """
-    tanh accept 1 parameter, the tanh layer will activate the neural unit.
+        """tanh accept 1 parameter, the tanh layer will activate the neural unit.
     @ formula:
     f_{tanh}(x) = tanh(x) = (exp(x)-exp(-x)) / (exp(x)+exp(-x))
     @ param:
@@ -211,8 +297,7 @@ class __Orange(object):
         self.__model.add(tf.keras.layers.Dense(neural_numbers,activation=tf.nn.tanh))
 
     def sigmoid(self,neural_numbers):
-        """
-    sigmoid accept 1 parameter, the sigmoid layer will activate the neural unit like relu.
+        """sigmoid accept 1 parameter, the sigmoid layer will activate the neural unit like relu.
     You can also use sigmoid on the lasted layer and set the neural number by 2 to output the result if your task is binary-
     classfication task.
     @ formula:
@@ -224,8 +309,7 @@ class __Orange(object):
 
 
     def softmax(self,neural_numbers):
-        """
-    softmax accept 1 parameter, softmax layer is always at the final layer in classfication task 
+        """softmax accept 1 parameter, softmax layer is always at the final layer in classfication task 
     to output the possibilities of each classes.
     @ formula:
     \sigma(z)_j = exp(z_j) / (_sum_{k=1}^K exp(z_k))
@@ -236,8 +320,7 @@ class __Orange(object):
         self.__model.add(tf.keras.layers.Dense(neural_numbers,activation=tf.nn.softmax))
     
     def bn(self,axis=1,momentum=0.99,epslion=1e-3):
-        """
-    bn accept three parameters, which all have default values. This three parameters are the most common parameter in
+        """bn accept three parameters, which all have default values. This three parameters are the most common parameter in
     batch normalization opertaion.
     For more details about batch normalization, see : 
     Batch Normalization: Accelerating Deep Network Training by Reducing Internal Covariate Shift
@@ -250,8 +333,7 @@ class __Orange(object):
         self.__model.add(tf.keras.layers.BatchNormalization(axis=axis,momentum=momentum,epslion=epslion))
     
     def Convolution(self,output_depth,kernel_size,x_stride,y_stride=None,padding='valid'):
-        """
-    Convolution is one of the most important operations in neural network. After convolutional operation the image can
+        """Convolution is one of the most important operations in neural network. After convolutional operation the image can
     get the new matrix set which called 'feature map'.
     @ param :
     output_depth : The depth of output feature map
@@ -275,8 +357,7 @@ class __Orange(object):
 
 
     def pooling(self,pool_size,strides,padding='valid',pool_type='maxpooling'):
-        """
-    Pooling layer is a layer to highlight the feature of feature map.
+        """Pooling layer is a layer to highlight the feature of feature map.
     @ param :
     pool_size : The size of pooling kernel, which is same as convolution operation. Pass a tuple if you want to pooling
     differently at different direction.
@@ -299,9 +380,86 @@ class __Orange(object):
             print('No such argument called : '% pool_type)
 
     """
-    Third part of the Orange library : Data preprocessing,callback function and model complication.
+    Third part of the Orange library : Data augmentation and preprocessing,callback function and model complication.
     """
+    def DataAugmentation(x_train=None,
+                         FeatureAverageSetZero=False,
+                         SampleAverageSetZero=False,
+                         FeatureDivideStddev=False,
+                         SampleDivideStddev=False,
+                         RotationRange=0,
+                         WidthShiftRange=0.0,
+                         HeightShiftRange=0.0,
+                         ZoomRange=0.0,
+                         HorizontalFilp=False,
+                         VerticalFilp=False,
+                         ReScale=None,
+                         ValidationSplit=0,
+        ):
+        """Before other preprocessing function, you should always call DataAugmentation() first.
+        DataAugmentation() doesn't enlarge the data immediately. After you call other function based on the object of ImageDataGenerator class,
+        the change will be applied. Returns a tf.keras.preprocessing.image.ImageDataGenerator class object.
+    @ param:
+    x_train -- The input data should be passed when FeatureAverageSetZero/SampleAverageSetZero/FeatureDivideStddev/SampleDivideStddev is True.
+    FeatureAverageSetZero -- Set each input 's average to 0, execute by each feature.
+    SampleAverageSetZero -- Set each sample 's average to 0.
+    FeatureDivideStddev -- Dividing each input by its standard deviation, execute by each feature.
+    SampleDivideStddev -- Dividing each sample by its standard deviation.
+    RotationRange -- An integer of the rotation angle range. The range is equal to [0,RotationRange]
+    WidthShiftRange -- You can pass a float/a 1-d array/an integer for image width shifting.
+                       \t Pass a float : Divide the width of image if pass a float < 1. Divide the pixel value if pass a float >=1
+                       \t Pass a 1-d array : Divide random element of the array.
+                       \t Pass an integer : The integer value may be chosen randomly at the range of (-WidthShiftRange,WidthShiftRange).
+    HeightShiftRnage -- The possible values are same as WidthShiftRange. This parameter is used for image height shifting.
+    ZoomRange -- A float or a list which shows the zoom range. If pass a float, the range is equal to [1-ZoomRange,1+ZoomRange].
+    HorizontalFilp -- Horizontal filp randomly.
+    VerticalFilp -- Vertical filp randomly.
+    ReScale -- The rescale factor. Image will have no change if pass None or 0. Otherwise the data will mutiple the rescale factor before any other convertion.
+    ValidationSplit -- The split ration of validation image. Must a float between 0 and 1.
+    """
+        datagen=tf.keras.preprocessing.image.ImageDataGenerator(featurewise_center=FeatureAverageSetZero,
+                                                                samplewise_center=SampleAverageSetZero,
+                                                                featurewise_std_normalization=FeatureDivideStddev,
+                                                                samplewise_std_normalization=SampleDivideStddev,
+                                                                rotation_range=RotationRange,
+                                                                width_shift_range=WidthShiftRange,
+                                                                height_shift_range=HeightShiftRange,
+                                                                zoom_range=ZoomRange,
+                                                                horizontal_filp=HorizontalFilp,
+                                                                vertical_filp=VerticalFilp,
+                                                                rescale=ReScale,
+                                                                validation_spilt=ValidationSplit
+            )
+        if FeatureAverageSetZero is True or SampleAverageSetZero is True or FeatureDivideStddev is True or SampleDivideStddev is True:
+            datagen.fit(x_train)
+        return datagen
 
+    def TransformForSingleImage(img,generator=None):
+        """TransfromForSingleImage, get a generator set by DataAugmentation() and apply it on single image. Returns an image with the same size of input.
+    @ param:
+    img -- The image need to be transformed.
+    generator -- The regulation of transformation, generated by DataAugmentation() or tf.keras.preprocessing.image.ImageDataGenerator.
+    """
+        if generator is None:
+            ExecutionError('The generator is invalid!')
+        if isinstance(generator,tf.keras.preprocessing.image.ImageDataGenerator):
+            img_after_transform=generator.apply_transform(img)
+        else:
+            ExecutionError('Please call DataAugmentation() first!')
+
+        return img_after_transform
+
+    def __Callback(param=[]):
+        """Callback function, a function which used in model execution.
+        The param is a list which can have this values:
+    @ param:
+    param -- a list contain the callback functions.
+    'NaN', TerminateOnNaN, if the Loss is too big, stop the training process.
+    'SaveCheckpoint', ModelCheckpoint, save the model after a epoch. If you pass the SaveCheckPoint, the next element of the param must be a dict which set 
+    the parameters of ModelCheckpoint.
+    'EarlyStopping', EarlyStopping, stop the training process when the monitored index doesn't increase/decrease.
+    'LearningRateChange', 
+    """
     
     """
     Fourth part of the Orange library : Visualization, image and video process.
@@ -320,7 +478,7 @@ class __Orange(object):
     @ param:
     path -- the path for the CIFAR-10 dataset.
     """
-        from urllib3 import HTTPError
+        from urllib import HTTPError
         try:
             (x_train,y_train),(x_test,y_test)=tf.keras.datasets.cifar10.load_data()
         except HTTPError:
@@ -351,7 +509,7 @@ class __Orange(object):
     path -- the path for the CIFAR-10 dataset.
     label_model -- 'fine' or 'coarse', means the label's accuracy (maybe?)
     """
-        from urllib3 import HTTPError
+        from urllib import HTTPError
         try:
             (x_train,y_train),(x_test,y_test)=tf.keras.datasets.cifar100.load_data(label_mode=label_mode)
         except HTTPError:
@@ -380,7 +538,7 @@ class __Orange(object):
         @ param:
         path -- the path for the IMDB dataset.
     """
-        from urllib3 import HTTPError
+        from urllib import HTTPError
         try:
             (x_train,y_train),(x_test,y_test)=tf.keras.datasets.imdb.load_data()
         except HTTPError:
@@ -396,7 +554,6 @@ class __Orange(object):
     # Put in OrangeLinear
 
 if __name__ == '__main__':
-    orange=__Orange("ResNet-18",(224,224))
     print('Usage : import Orange')
     print('Or from Orange import OrangeLinear')
     print('Or from Orange import OrangeNonLinear')
